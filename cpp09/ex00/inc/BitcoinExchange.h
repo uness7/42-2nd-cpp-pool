04d6df85eb2e63430359767ca6c3480420c6e6a5
Youness Zioual <crness2@gmail.com>
2024-11-27 20:56:23 +0100

    add cpp09/ex00

---
+++ cpp09/ex00/inc/BitcoinExchange.h
@@ -0,0 +1,14 @@
+# ifndef BITCOINEXCHANGE_H
+# define BITCOINEXCHANGE_H
+
+# include <iostream>
+# include <algorithm>
+# include <string>
+# include <map>
+
+class BitcoinExchange {
+	public:
+		std::map<std::string, int>	name_age;
+};
+
+# endif
